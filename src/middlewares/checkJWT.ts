437dacc2fdcf3736103e6204b5bb0f959bf01be2
Lais Frigério <laiss@gazetadopovo.com.br>
2019-11-19 07:54:30 -0200

    Ajuste check authorization token

--- src/middlewares/checkJWT.ts
+++ src/middlewares/checkJWT.ts
@@ -1,13 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import { getRepository } from "typeorm";
 import jwt  from "jsonwebtoken";
+import User from "../entity/user";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        //req.body.user = decoded.user;
-        next();
+        const user = (decoded as User);
+
+        const findUser = await getRepository(User).findOne(user.id);
+        if (findUser) {
+            req.body.user = user;
+            return next();
+        }
+
+        return res.status(404).send({
+            auth: false,
+            message: "User not found"
+        });
     });
 }
