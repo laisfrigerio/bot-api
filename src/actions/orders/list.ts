a366cb8501e07393f98fc6edbe3df424195840da
Lais Frigério <laiss@gazetadopovo.com.br>
2019-11-19 07:54:50 -0200

    List orders (compras)

--- src/actions/orders/list.ts
+++ src/actions/orders/list.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Order from "../../entity/order";
 
 export default class List {
     public async do(req: Request, res: Response) {
         try {
+            const response = await getRepository(Order).find();
+            return res.json(response);
             //- List codigo, valor, data, % de cachback aplicado para esta compra, valor de cashback para a próxima compra e status
         } catch (ex) {
             //
         }
+
+        return res.json([]);
     }
 }
